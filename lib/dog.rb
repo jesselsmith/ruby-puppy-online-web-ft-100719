61873c629de8f3450c29fedc0937cb9a8aa3bdc4
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-11 03:59:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,13 +6,21 @@ class Dog
     self.name = name
     self.save
   end
+
   def save
     @@all << self
   end
+
   def self.all
     @@all
   end
+
   def self.clear_all
     @@all.clear
   end
+
+  def self.print_all
+    @@all.each{|dog| puts dog.name}
+  end
+  
 end
